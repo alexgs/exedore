029fdab7950269cdbcabaffff77ef9e26a5bad79
Phillip Gates-Shannon <phil.gs@gmail.com>
2016-07-02 15:50:49 -0400

    Add "wrapClassMethod" for working with ES6 class methods
    Of course, ES6 "class methods" are just methods defined on the "constructor" function's prototype, so this will work with the class pattern from ES5 and earlier.

--- src/index.js
+++ src/index.js
@@ -23,12 +23,31 @@ let Exedore = {
         } );
     },
 
+    // beforeClassMethod: function( targetConstructor, functionName, advice ) {
+    //     Exedore.wrap( targetConstructor.prototype, functionName, function( originalFunction, args ) {
+    //         advice.apply( this, [ originalFunction, args ] );
+    //         return Exedore.next( this, originalFunction, args );
+    //     } );
+    // },
+
     next: function( contextObject, functionRef, args = [ ] ) {
         return functionRef.apply( contextObject, args );
     },
 
     wrap: function( targetObject, functionName, advice ) {
         Exedore.around( functionName, advice, targetObject );
+        // TODO Experiment with the original "match" function from Aop.js
+        // If we go back to the original "match" function from Aop.js, can we
+        // wrap loose functions (e.g. private functions in a module) as well as
+        // functions that are attached to an object?
+    },
+
+    wrapClassMethod: function( targetClass, functionName, advice ) {
+        let originalMethod = targetClass.prototype[ functionName ];
+        targetClass.prototype[ functionName ] = function() {
+            let args = Array.from( arguments );
+            return advice.apply( this, [ originalMethod, args ] );
+        }
     }
 };
 
